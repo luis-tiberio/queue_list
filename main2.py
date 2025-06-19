0d3203319f520bd05e2612cd091eda9f3fbda039
luis-tiberio <luisotaviotiberio@gmail.com>
2025-06-18 22:10:44 -0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -111,6 +111,7 @@ async def get_data(page, download_dir):
         await page.locator('xpath=/html/body/div[5]/div[2]/div/div/div[4]/div[2]/button[2]/span').click()
         await page.wait_for_timeout(15000)
 
+        """
         # 👉 Botão de download
         async with page.expect_download() as download_info:
             await page.locator('xpath=/html/body/span/div/div[1]/div/span/div/div[2]/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/button/span').click()
@@ -119,6 +120,16 @@ async def get_data(page, download_dir):
         await download.save_as(download_path)
         time.sleep(2)  # Pequeno delay para garantir o arquivo no sistema
         rename_downloaded_file(download_dir)
+        """
+
+        # Use async with para download
+        async with page.expect_download() as download_info:
+            #await page.wait_for_selector('xpath=/html/body/span/div/div[1]/div/span/div/div[2]/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/button', timeout=30000)
+            await page.locator('xpath=/html[1]/body[1]/span[1]/div[1]/div[1]/div[1]/span[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/button[1]/span[1]').click()
+        download = await download_info.value
+        download_path = os.path.join(DOWNLOAD_DIR, download.suggested_filename)
+        await download.save_as(download_path)
+        new_file_path = rename_downloaded_file(download_dir)
 
     except Exception as e:
         print(f"Erro ao coletar dados: {e}")
